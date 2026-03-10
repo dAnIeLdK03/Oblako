3aefb4095f3836a70abb6c68fcf32ad21849b78d
Daniel <danikrustanov@gmail.com>
2026-03-10 10:33:48 +0200

    Migrating to Node 20+ and synchronizing dependency versions

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import CookieConsent from './components/CookieConsent.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WeatherTips from './pages/WeatherTips.jsx';
 
+
 function App() {
   return (
     <Router>
